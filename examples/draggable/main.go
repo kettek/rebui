c4a208e1f369f459e1492cba301e3baa6682b22c
kettek <kts@kettek.net>
2025-02-24 16:31:14 -0800

    Make draggable a little more interactive

--- examples/draggable/main.go
+++ examples/draggable/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image/color"
 	"log"
 	"os"
 
@@ -37,6 +38,7 @@ func main() {
 	}
 
 	g.layout = layout
+	g.layout.ClampPointers = true
 	g.layout.Generate()
 
 	ebiten.SetWindowSize(320, 240)
@@ -47,10 +49,35 @@ func main() {
 	}
 }
 
+// DraggableButton is a button that can be dragged around.
 type DraggableButton struct {
 	widgets.Button
+	pressCount int
 }
 
+// HandlePointerPress receives a press event if the pointer is over the element.
+func (b *DraggableButton) HandlePointerPress(e rebui.PointerPressEvent) {
+	b.pressCount++
+	b.SetBackgroundColor(color.NRGBA{255, 0, 0, 255})
+}
+
+// HandlePointerRelease receives a release event if the pointer is over the element.
+func (b *DraggableButton) HandlePointerRelease(e rebui.PointerReleaseEvent) {
+	b.pressCount--
+	if b.pressCount <= 0 {
+		b.SetBackgroundColor(rebui.CurrentTheme().BackgroundColor)
+	}
+}
+
+// HandlePointerGlobalRelease receives a release event _if_ the element was initially pressed, but did not receive a release over it.
+func (b *DraggableButton) HandlePointerGlobalRelease(e rebui.PointerReleaseEvent) {
+	b.pressCount--
+	if b.pressCount <= 0 {
+		b.SetBackgroundColor(rebui.CurrentTheme().BackgroundColor)
+	}
+}
+
+// HandlePointerGlobalMove receives a move event if the pointer received the a press event. Note that this will receive move events _per_ pointer, so 3 mouse buttons will move the element at 3 times delta.
 func (b *DraggableButton) HandlePointerGlobalMove(e rebui.PointerMoveEvent) {
 	// Ignore default move event.
 	if e.PointerID == -1 {
