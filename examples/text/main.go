f31a4aeea703d17f871c4355813f261bfdf7e617
kettek <kts@kettek.net>
2025-03-01 01:08:49 -0800

    Add alignment buttons of testing

--- examples/text/main.go
+++ examples/text/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kettek/rebui"
 	_ "github.com/kettek/rebui/defaults/font"
+	"github.com/kettek/rebui/widgets"
 	_ "github.com/kettek/rebui/widgets"
 )
 
@@ -28,9 +29,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	g := &Game{}
+	g.setupButtons()
 
 	g.layout.AddNode(rebui.Node{
 		Type:            "Text",
+		ID:              "text",
 		Width:           "50%",
 		Height:          "50%",
 		X:               "50%",
@@ -51,5 +54,80 @@ func main() {
 
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
+	}
+}
+
+// These are just the buttons for controlling the alignment of the text.
+func (g *Game) setupButtons() {
+	g.layout.AddNode(rebui.Node{
+		Type:   "Button",
+		ID:     "button1",
+		Width:  "25%",
+		Height: "30",
+		Text:   "Left",
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetHorizontalAlignment(rebui.AlignLeft)
+	}
+	g.layout.AddNode(rebui.Node{
+		Type:            "Button",
+		ID:              "button2",
+		Width:           "25%",
+		Height:          "30",
+		X:               "after button1",
+		Text:            "Center",
+		HorizontalAlign: rebui.AlignCenter,
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetHorizontalAlignment(rebui.AlignCenter)
+	}
+	g.layout.AddNode(rebui.Node{
+		Type:            "Button",
+		ID:              "button3",
+		Width:           "25%",
+		Height:          "30",
+		X:               "after button2",
+		Text:            "Right",
+		HorizontalAlign: rebui.AlignRight,
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetHorizontalAlignment(rebui.AlignRight)
+	}
+	g.layout.AddNode(rebui.Node{
+		Type:   "Button",
+		ID:     "button4",
+		Width:  "25%",
+		Height: "30",
+		Y:      "after button1",
+		Text:   "Top",
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetVerticalAlignment(rebui.AlignTop)
+	}
+	g.layout.AddNode(rebui.Node{
+		Type:          "Button",
+		ID:            "button5",
+		Width:         "25%",
+		Height:        "30",
+		X:             "after button4",
+		Y:             "after button2",
+		Text:          "Middle",
+		VerticalAlign: rebui.AlignMiddle,
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetVerticalAlignment(rebui.AlignMiddle)
+	}
+	g.layout.AddNode(rebui.Node{
+		Type:          "Button",
+		ID:            "button6",
+		Width:         "25%",
+		Height:        "30",
+		X:             "after button5",
+		Y:             "after button3",
+		Text:          "Bottom",
+		VerticalAlign: rebui.AlignBottom,
+	}).OnPointerPressed = func(e rebui.EventPointerPressed) {
+		txt := g.layout.GetByID("text").Widget.(*widgets.Text)
+		txt.SetVerticalAlignment(rebui.AlignBottom)
 	}
 }
