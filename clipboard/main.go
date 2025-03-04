049ed7a470e32ce2d80126b153849e5c98e36e8a
kettek <kts@kettek.net>
2025-03-03 20:43:35 -0800

    Add comments

--- clipboard/main.go
+++ clipboard/main.go
@@ -5,18 +5,22 @@ var text string
 var textGetter func() string
 var textSetter func(string)
 
+// SetTextGetter sets the function that is used to acquire the current clipboard contents.
 func SetTextGetter(f func() string) {
 	textGetter = f
 }
 
+// SetTextSetter sets the function that is used to set the current clipboard contents.
 func SetTextSetter(f func(string)) {
 	textSetter = f
 }
 
+// GetText returns the contents of the clipboard.
 func GetText() string {
 	return textGetter()
 }
 
+// SetText sets the contents of the clipboard.
 func SetText(s string) {
 	textSetter(s)
 }
